d59b147d5ad5567c8a83c3db79e10be47976a20f
Jaykishan Hirpara <jaykishan@supertax.in>
2019-02-08 16:39:09 +0530

    Updates

---
+++ precache-manifest.a1e55e2a760e0974c8d926f9fddb90eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e3cf5f8c46f840644cb",
+    "url": "/jaykishanss23/static/js/runtime~main.7e3cf5f8.js"
+  },
+  {
+    "revision": "38e682e21bbc07c3dbfd",
+    "url": "/jaykishanss23/static/js/main.38e682e2.chunk.js"
+  },
+  {
+    "revision": "3bf8d718a66535446819",
+    "url": "/jaykishanss23/static/js/1.3bf8d718.chunk.js"
+  },
+  {
+    "revision": "38e682e21bbc07c3dbfd",
+    "url": "/jaykishanss23/static/css/main.8eb08f91.chunk.css"
+  },
+  {
+    "revision": "04c6ca2a63d36f6e4d0624865b1fb956",
+    "url": "/jaykishanss23/index.html"
+  }
+];
